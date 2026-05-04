b4a9cc2601c733909953519811615c4af3fb9452
jhefer10 <jhefer9210@gmail.com>
2026-05-04 18:13:35 +0000

    Agregar modelos de base de datos y comando para poblar datos de ejemplo

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,12 +1,14 @@
 from djongo import models
 
 class Team(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100, unique=True)
     universe = models.CharField(max_length=50)
     def __str__(self):
         return self.name
 
 class User(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
@@ -14,6 +16,7 @@ class User(models.Model):
         return self.name
 
 class Activity(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
     type = models.CharField(max_length=50)
     duration = models.PositiveIntegerField()
@@ -22,6 +25,7 @@ class Activity(models.Model):
         return f"{self.user.name} - {self.type}"
 
 class Workout(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100)
     description = models.TextField()
     suggested_for = models.ManyToManyField(Team, related_name='workouts')
@@ -29,6 +33,7 @@ class Workout(models.Model):
         return self.name
 
 class Leaderboard(models.Model):
+    _id = models.ObjectIdField(primary_key=True, editable=False)
     team = models.OneToOneField(Team, on_delete=models.CASCADE, related_name='leaderboard')
     points = models.PositiveIntegerField(default=0)
     def __str__(self):
